187ee5c47a1409f617db0843bf48ac3506ce34ed
Stefano <stefano.visentin@anyt1me.com>
2024-12-28 11:50:56 +0100

    🐛 env varibale name fixed

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/lib/types';
 
-const supabaseUrl = process.env.NEXT_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
